e776d4035ceb01d9cf8190a2f112e4a67efe6851
Yash <yashvaibhav19@gmail.com>
2022-12-26 12:03:07 +0530

    complete dark mode

---
+++ Dark mode/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import DarkMode from './02-dark-mode/DarkMode'
+
+function App () {
+  return (
+    <div className='App'>
+      <DarkMode />
+    </div>
+  )
+}
+
+export default App
