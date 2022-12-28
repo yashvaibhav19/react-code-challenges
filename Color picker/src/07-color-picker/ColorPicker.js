1c733955a194d72de7e59c4c358e9aa6f4647a87
Yash <yashvaibhav19@gmail.com>
2022-12-28 13:28:50 +0530

    enable color change

--- Color picker/src/07-color-picker/ColorPicker.js
+++ Color picker/src/07-color-picker/ColorPicker.js
@@ -22,7 +22,17 @@ export default function ColorPicker () {
   const [backgroundColor, setBackgroundColor] = useState('white')
 
   return (
-    <div className='page' style={{ backgroundColor }}>
+    <div className='page' style={{ backgroundColor }} onClick={(e)=>{
+      e.preventDefault();
+      const colorReq = (e.target.textContent);
+      colors.forEach((obj)=>{
+        if(obj.name === colorReq)
+        {
+          setBackgroundColor(obj.hex)
+          return true;
+        }
+      })
+    }}>
       {
         colors.map(color => (
           <Color key={color.hex} hex={color.hex} name={color.name} />
